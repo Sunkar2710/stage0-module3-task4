19bdc06ae19b6643fbece7e162f56c7d01281067
Сункар Бауыржанулы <sunkar2710@gmail.com>
2023-04-18 12:02:42 +0600

    feat: implement stage0-module3-task4

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char charC = 99;
+        char charH = 104;
+        char charA = 97;
+        char charR = 114;
+        System.out.println(charC + "\n" + charH + "\n" + charA + "\n" + charR);
     }
 }
